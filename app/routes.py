899f291e2658615c0e490cee4707205de519f374
Ujual PS <ujual.ps@hashedin.com>
2020-03-03 15:04:35 +0530

    login api added.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,40 @@
 from flask import request, Flask
 from app import app, db
 from app.models import User
+from flask_httpauth import HTTPBasicAuth
 
 
-@app.route('/users', methods=['GET', 'POST'])
+auth = HTTPBasicAuth()
+
+
+@app.route('/login', methods=['POST'])
+def login_method():
+    response = request.get_json()
+
+    email = response['email']
+    password = response['password']
+
+    user = User.query.filter_by(email=email).first()
+    if user.password_hash == password:
+        return str(user.id)
+    else:
+        return "0"
+    # try:
+    #     user = User.query.filter_by(email=email).first()
+    #     if user.password_hash == password:
+    #         return '1'
+    #     else:
+    #         return '2'
+    # finally:
+    #     return email
+
+
+
+
+
+
+
+@app.route('/users', methods=['POST'])
 def user_methods():
     response = request.get_json()
 
